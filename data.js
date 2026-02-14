f90ff03b8b66f6ef2e87d961f0f3ba2324ef62f5
lufemattos <lufesgmattos@gmail.com>
2026-02-14 03:05:29 -0300

    Update data.js

--- data.js
+++ data.js
@@ -27,4 +27,18 @@ window.REPORT = {
       text: "Demand Planning 3YLP Forecast"
     }
   ]
+
+  timeline: [
+  { side: "top",    date: "20/01", color: "#E97132", boxType: "dark",    title: "X-MPU",        subtitle: "20-22 Jan" },
+  { side: "bottom", date: "30/01", color: "#60A5FA", boxType: "light",   title: "Abertura",     subtitle: "Portal" },
+  { side: "top",    date: "16/02", color: "#22C55E", boxType: "light",   title: "Carnaval",     subtitle: "16-18 Fev" },
+  { side: "bottom", date: "02/03", color: "#FACC15", boxType: "light",   title: "Fechamento",   subtitle: "Portal" },
+  { side: "top",    date: "03/03", color: "#FB923C", boxType: "light",   title: "Bookings",     subtitle: "Prévia" },
+  { side: "bottom", date: "04/03", color: "#F472B6", boxType: "light",   title: "Booking",      subtitle: "Review" },
+  { side: "top",    date: "05/03", color: "#A855F7", boxType: "light",   title: "Fechamento",   subtitle: "Final" },
+  { side: "bottom", date: "09/03", color: "#2DD4BF", boxType: "light",   title: "Bookings",     subtitle: "Finais" },
+  { side: "top",    date: "12/03", color: "#F87171", boxType: "light",   title: "Alinhamento",  subtitle: "Liderança" },
+  { side: "bottom", date: "18/03", color: "#EAB308", boxType: "light",   title: "POs Deadline", subtitle: "SAP Nike" }
+],
+  
 };
