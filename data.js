78a73903e9ecd6837bc57231ba238bc43201e887
lufemattos <lufesgmattos@gmail.com>
2026-02-14 02:40:36 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,30 @@
+window.REPORT = {
+  lastUpdate: "Fev/2026",
+
+  justTrackIt: [
+    {
+      accent: true,
+      title: "HO26 Bottoms Up Forecast:",
+      text: "6,82M TOTAL = 3,21M APP + 618K EQP + 3,00M FTW / 3,48M IMP + 3,34M L4L"
+    },
+    {
+      date: "4.3",
+      text: "DEADLINE BOOKINGS HO26 (prévia) – MPO",
+      warn: true
+    },
+    {
+      date: "5.3",
+      text: "DEADLINE BOOKINGS REVIEW FEEDBACK – MERCH",
+      warn: true
+    },
+    {
+      date: "9.3",
+      text: "DEADLINE BOOKINGS HO26 (final) – MPO",
+      warn: true
+    },
+    {
+      date: "3.4",
+      text: "Demand Planning 3YLP Forecast"
+    }
+  ]
+};
